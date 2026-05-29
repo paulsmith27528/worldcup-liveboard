0b2b218e63e1b11b8395d41ead9b92472506436f
paulsmith27528 <paulsmith27528@outlook.com>
2026-05-29 20:35:20 +0100

    Update MatchCard.tsx

--- components/MatchCard.tsx
+++ components/MatchCard.tsx
@@ -1,3 +1,26 @@
-export default function MatchCard() {
-  return null;
+type Props = {
+  home: string;
+  away: string;
+  homeFlag: string;
+  awayFlag: string;
+  homeScore: number;
+  awayScore: number;
+};
+
+export default function MatchCard({
+  home,
+  away,
+  homeFlag,
+  awayFlag,
+  homeScore,
+  awayScore,
+}: Props) {
+  return (
+    <div>
+      <strong>
+        {homeFlag} {home} {homeScore} - {awayScore} {awayFlag} {away}
+      </strong>
+      <div>LIVE 78&apos;</div>
+    </div>
+  );
 }
