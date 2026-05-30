2cdecd2d27704dea876dc2bb7a85accc7ffa46e0
paulsmith27528 <paulsmith27528@outlook.com>
2026-05-30 12:19:19 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
       <header style={topHeader}>
         <div style={logoBlock}>
           <img
-  src="/world-cup-trophy.png"
+  src="world-cup-trophy.png"
   alt="FIFA World Cup"
   style={{
     height: "48px",
