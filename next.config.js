1d5c4a294cb4bec126964a8e3513d3da2a7a3831
paulsmith27528 <paulsmith27528@outlook.com>
2026-06-05 21:44:29 +0100

    Redirect root URL to landing.html

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landing.html',
+        permanent: false,
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
